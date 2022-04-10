d74f1841c12a3147cf093d7c333802a0665ba1b5
Matthieu <matthieu.ranucci@ekino.com>
2022-04-10 21:29:51 +0200

    Display data in BookmarkItem

--- src/components/BookmarkItem/BookmarkItem.tsx
+++ src/components/BookmarkItem/BookmarkItem.tsx
@@ -21,10 +21,39 @@ type BookmarkItemProps = {
 export default function BookmarkItem({ bookmark }: BookmarkItemProps) {
   const { deleteBookmark } = useContext(BookmarkContext);
 
+  const {
+    author_name,
+    duration,
+    height,
+    id,
+    provider_name,
+    thumbnail_url,
+    title,
+    upload_date,
+    url,
+    width,
+  } = bookmark;
+
+  const isVimeo = provider_name === "Vimeo";
+  const isFlickr = provider_name === "Flickr";
+
   return (
     <li>
-      BookmarkItem
-      <button onClick={() => deleteBookmark(bookmark.id)}>Delete</button>
+      <>
+        {thumbnail_url && <img src={thumbnail_url} alt={title} />}
+
+        <a href={url} rel="noreferrer noopener" target="_blank">
+          {url && <span>{`Url: ${url}`}</span>}
+        </a>
+        {title && <span>{`Title: ${title}`}</span>}
+        {author_name && <span>{`Author: ${author_name}`}</span>}
+        {upload_date && <span>{`UploadDate: ${upload_date}`}</span>}
+        {isVimeo && duration && <span>{`duration: ${duration}`}</span>}
+        {isFlickr && width && height && (
+          <span>{`Dimension: ${width} x ${height}`}</span>
+        )}
+      </>
+      <button onClick={() => deleteBookmark(id)}>Delete</button>
     </li>
   );
 }
